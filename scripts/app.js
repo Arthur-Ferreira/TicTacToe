c07f478efd4a4c5f7033df8fe25b17f08d2a4b13
Arthur-Ferreira <arthufdarosa@gmail.com>
2023-05-02 18:21:25 -0300

    Project Finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,56 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+]
+const players = [
+  {
+    name: '',
+    symbol: 'X'
+  },
+  {
+    name: '',
+    symbol: 'O'
+  },
+]
+
+
+const formElement = document.querySelector('form')
+const errorsOutputElement = document.getElementById('config-errors')
+const gameFieldElement = document.getElementById('game-field')
+const activePlayerNameElement = document.getElementById('active-player-name')
+const gameOverElement = document.getElementById('game-over')
+
+
 const configOverlayElement = document.getElementById('config-overlay')
 const backdropElement = document.getElementById('backdrop')
-const cancelConfigBtnElement = document.getElementById('cancel-config-btn')
+
 
 const editPlayer1Btn = document.getElementById('edit-player-1-name')
 const editPlayer2Btn = document.getElementById('edit-player-2-name')
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn')
+const startGameBtnElement = document.getElementById('start-game-btn')
+
+
+const gameFieldItems = document.querySelectorAll('#game-board li')
+
+
+let editedPlayer = 0
+let activePlayer = 0
+let currentRound = 1
+let gameIsOver = false
+
 
 
 editPlayer1Btn.addEventListener('click', openPlayerCondig)
 editPlayer2Btn.addEventListener('click', openPlayerCondig)
+startGameBtnElement.addEventListener('click', startNewGame)
 
 cancelConfigBtnElement.addEventListener('click', closePlayerCondig)
 backdropElement.addEventListener('click', closePlayerCondig)
+
+formElement.addEventListener('submit', savePlayerConfig)
+
+gameFieldItems.forEach(item => {
+  item.addEventListener('click', selectGameField)
+})
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerCondig() {
+function openPlayerCondig(event) {
+  editedPlayer = +event.target.dataset.playerid
+
   configOverlayElement.style.display = 'block'
   backdropElement.style.display = 'block'
 }
@@ -6,4 +8,27 @@ function openPlayerCondig() {
 function closePlayerCondig() {
   configOverlayElement.style.display = 'none'
   backdropElement.style.display = 'none'
+  formElement.firstElementChild.classList.remove('none')
+  errorsOutputElement.textContent = ''
+  formElement.firstElementChild.lastElementChild.value = ''
+}
+
+
+function savePlayerConfig(event) {
+  event.preventDefault()
+
+  const formData = new FormData(event.target)
+  const enteredPlayername = formData.get('playername').trim()
+  const updatedPlayerDataElement = document.getElementById(`player-${editedPlayer}-data`)
+
+  if (!enteredPlayername) { // enteredPlayer === ''
+    event.target.firstElementChild.classList.add('error')
+    errorsOutputElement.textContent = 'Please enter a valid name!'
+    return
+  }
+
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername
+
+  players[editedPlayer - 1].name = enteredPlayername
+  closePlayerCondig()
 }
